4c8310734cd7f97818d383d41090cca947187630
Kyle Westbroek <Kylewestbroek@mail.weber.edu>
2022-08-03 20:35:03 -0600

    adjusted img arr for netlify again

--- src/javascripts/hatArr.js
+++ src/javascripts/hatArr.js
@@ -3,27 +3,27 @@ export const hats_Gal = [{
         "title": "Basic ridding hat",
         "year": 1844,
         "description": "Blak pointy ridding hat. you choice of feathers",
-        "pic": "./images/image1405.jpg"
+        "pic": "/image1405.jpg"
     },
     {
         "id": 2,
         "title": "Bonnet",
         "year": 1820,
         "description": "classic Bonnet and bow.",
-        "pic": "./images/image1409.jpg"
+        "pic": "/image1409.jpg"
     },
     {
         "id": 3,
         "title": "Riding hat with Veil",
         "year": 1866,
         "description": "Riding hat with veril.",
-        "pic": "./images/image1528.jpg"
+        "pic": "/images/image1528.jpg"
     },
     {
         "id": 4,
         "title": "Elegant ridding hat",
         "year": 1860,
         "description": "Elegant ridding hat. the feather can be replaced with a flower if needed.",
-        "pic": "./images/image1497.jpg"
+        "pic": "/image1497.jpg"
     }
 ]
